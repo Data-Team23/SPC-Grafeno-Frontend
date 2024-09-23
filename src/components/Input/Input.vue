4c9c24223b3a86e80ec9c933dd1d530be869decd
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-09-23 14:22:51 -0300

    feat: add service create user and add terms of lgpd and add mask for input camps

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,13 +1,14 @@
 <template>
     <label class="label">
         <i :class="icon"></i>
-        <input class="input" :type="type" :value="modelValue" :placeholder="placeholder"
+        <input class="input" :type="type" :value="maskedValue" :placeholder="placeholder"
             @input="updateInput">
     </label>
 </template>
 
 <script>
-import "./styles.css"
+import "./styles.css";
+
 export default {
     name: "Input",
     props: {
@@ -30,11 +31,44 @@ export default {
         icon: {
             type: String,
             default: "fa-solid fa-envelope"
+        },
+        mask: {
+            type: String,
+            default: ""
+        }
+    },
+    computed: {
+        maskedValue() {
+            return this.applyMask(this.modelValue, this.mask);
         }
     },
     methods: {
         updateInput(event) {
-            this.$emit("update:modelValue", event.target.value)
+            let value = event.target.value;
+            if (this.mask) {
+                value = this.removeMask(value);
+            }
+            this.$emit("update:modelValue", value);
+        },
+        applyMask(value, mask) {
+            if (!mask) return value;
+            const cleanedValue = value.replace(/\D/g, '');
+
+            if (mask === "(##) #####-####") {
+                return cleanedValue.replace(/(\d{2})(\d)/, '($1) $2')
+                                   .replace(/(\d{5})(\d)/, '$1-$2');
+            } else if (mask === "(##) ####-####") {
+                return cleanedValue.replace(/(\d{2})(\d)/, '($1) $2')
+                                   .replace(/(\d{4})(\d)/, '$1-$2');
+            } else if (mask === "###.###.###-##") {
+                return cleanedValue.replace(/(\d{3})(\d)/, '$1.$2')
+                                   .replace(/(\d{3})(\d)/, '$1.$2')
+                                   .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+            }
+            return value;
+        },
+        removeMask(value) {
+            return value.replace(/\D/g, '');
         }
     }
 }
