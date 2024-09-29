bcf51dc928d863e72b5990a27629beddf5ceb444
Tobias Sousa <100934496+tobiassousa@users.noreply.github.com>
2024-09-29 13:44:44 -0300

    feat: add powerBi link

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 style="margin-left: 500px; color: black;">DASHBOARD</h1>
+    <h1 style="margin-left: 100px; color: black;">DASHBOARD</h1>
     <iframe title="API" width="90%" height="700" src="https://app.powerbi.com/reportEmbed?reportId=42b14e2d-f74f-4f12-b30e-96f7551782cb&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" style="margin-left: 120px;" frameborder="0" allowFullScreen="true"></iframe>
 
     
