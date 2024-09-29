34432d8718974512710361b3e0cea6ce53e20658
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-09-29 13:44:11 -0300

    chore: add date formatting

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -4,7 +4,7 @@
         <br>
         <br>
         <Table
-            :data="data"
+            :data="formattedData"
             :display-columns="['email', 'first_name', 'cpf', 'contato', 'created_at', 'updated_at']"
             :column-names="['Email', 'Nome', 'CPF', 'Contato', 'Criado em:', 'Atualizado em:']"
             :show-delete-column="true">
@@ -13,7 +13,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import Table from "../components/Table/Table.vue";
 import { userApi } from "@/services/api";
 import { toast } from "vue3-toastify";
@@ -29,6 +29,19 @@ const fetchUsers = async () => {
         toast.error("Erro ao carregar usuários.");
     }
 };
+
+const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false };
+    return new Intl.DateTimeFormat("pt-BR", options).format(new Date(dateString));
+};
+
+const formattedData = computed(() => {
+    return data.value.map(user => ({
+        ...user,
+        created_at: formatDate(user.created_at),
+        updated_at: formatDate(user.updated_at)
+    }));
+});
 
 onMounted(() => {
     fetchUsers();
