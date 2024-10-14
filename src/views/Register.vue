509ffba7d1fce0bd24d7d2dee1cd981f0dee7da2
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-10-13 22:27:41 -0300

    refactor: improve CSS styling for terms section and reduce spacing for clarity

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -25,47 +25,70 @@
                 <Input placeholder="Contato" icon="fa-solid fa-phone" v-model="contact" :mask="'(##) #####-####'" />
                 <Input placeholder="Senha" icon="fa-solid fa-lock" type="password" v-model="password" />
                 <Input placeholder="Confirmação senha" icon="fa-solid fa-lock" type="password" v-model="confirmPassword" />
-                
                 <div class="terms-checkbox">
                     <input type="checkbox" v-model="acceptedTerms" id="terms" class="styled-checkbox"/>
-                    <label class="terms-label" for="terms"> Li e aceito os termos de uso e a política de privacidade. <span @click="openModal" class="link">Leia aqui</span></label>
+                    <label class="terms-label" for="terms"> Li e aceito os termos de uso e a política de privacidade.</label>
+                    <span @click="openModal" class="link">Leia aqui</span>
                 </div>
-                
                 <Button textButton="CADASTRAR"></Button>
             </form>
             <Footer></Footer>
         </div>
         <div v-if="showModal" class="modal-overlay">
             <div class="modal">
-                <button @click="closeModal" class="close-button custom-close">X</button>
                 <h2>Termos de Uso e Política de Privacidade</h2>
                 <p>
-                    Ao criar uma conta, você concorda com os seguintes termos: 
-                    Seus dados serão utilizados exclusivamente para fins de cadastro e 
-                    aprimoramento de nossos serviços, respeitando a Lei Geral de Proteção de Dados (LGPD).
-                    Você tem o direito de acessar, corrigir e excluir seus dados a qualquer momento.
+                    Ao criar uma conta em nossa plataforma, você concorda com os seguintes termos e condições:
+                    <br><br>
+                    1. <strong>Uso de Dados Pessoais</strong>: Seus dados pessoais, como nome, CPF, e-mail e número de contato, serão coletados e utilizados exclusivamente para fins de registro e para melhorar a experiência do usuário em nossa plataforma. 
+                    <br><br>
+                    2. <strong>Finalidade do Tratamento dos Dados</strong>: Os dados coletados podem ser utilizados para:
+                    <span class="reduced-line-spacing">
+                        <br>
+                        - Comunicação sobre novidades, atualizações e ofertas;
+                        <br>
+                        - Personalização de conteúdos e anúncios;
+                        <br>
+                        - Melhoria de nossos serviços com base em suas preferências;
+                        <br>
+                        - Atendimento ao cliente e suporte.
+                    </span>
+                    <br><br>
+                    3. <strong>Direitos do Usuário</strong>: Você tem o direito de acessar, corrigir ou excluir seus dados pessoais a qualquer momento. Para isso, entre em contato conosco através dos canais disponibilizados em nossa plataforma.
+                    <br><br>
+                    4. <strong>Cookies e Tecnologias Similares</strong>: Utilizamos cookies e tecnologias semelhantes para garantir a melhor experiência em nosso site. Você pode gerenciar suas preferências de cookies nas configurações do seu navegador.
+                    <br><br>
+                    5. <strong>Revogação do Consentimento</strong>: Você pode revogar seu consentimento a qualquer momento. Entretanto, é importante observar que, ao revogar o consentimento, sua conta será excluída, e você perderá acesso a todos os dados e serviços associados.
+                    <br><br>
+                    6. <strong>Modificações nos Termos</strong>: Reservamo-nos o direito de modificar estes termos a qualquer momento. Quaisquer alterações serão informadas através de um aviso em nossa plataforma, e a continuidade do uso dos nossos serviços será considerada como aceitação dos novos termos.
+                    <br><br>
+                    Ao clicar em "Aceitar", você concorda com todos os termos acima. Se não concordar, você não poderá criar uma conta em nossa plataforma.
                 </p>
+                <div class="modal-buttons">
+                    <button @click="acceptTerms" class="accept-button">Aceitar</button>
+                    <button @click="closeModal" class="reject-button">Recusar</button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import Input from '../components/Input/Input.vue';
-import Button from '../components/Button/Button.vue';
-import Footer from '../components/Footer/Footer.vue';
+import Input from "@/components/Input/Input.vue";
+import Button from "@/components/Button/Button.vue";
+import Footer from "@/components/Footer/Footer.vue";
 import "../assets/styles.css";
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { toast } from "vue3-toastify";
 import { userApi } from "@/services/api";
 
-const name = ref('');
-const cpf = ref('');
-const email = ref('');
-const contact = ref('');
-const password = ref('');
-const confirmPassword = ref('');
+const name = ref("");
+const cpf = ref("");
+const email = ref("");
+const contact = ref("");
+const password = ref("");
+const confirmPassword = ref("");
 const acceptedTerms = ref(false);
 const showModal = ref(false);
 const router = useRouter();
@@ -91,16 +114,16 @@ async function register() {
         return;
     }
 
-    const nameParts = name.value.split(' ');
+    const nameParts = name.value.split(" ");
     const firstName = nameParts[0];
     const lastName = nameParts[nameParts.length - 1];
 
     const data = {
         first_name: firstName,
         last_name: lastName,
-        cpf: cpf.value.replace(/\D/g, ''),
+        cpf: cpf.value.replace(/\D/g, ""),
         email: email.value,
-        contato: contact.value.replace(/\D/g, ''),
+        contato: contact.value.replace(/\D/g, ""),
         password: password.value
     };
 
@@ -124,6 +147,16 @@ function openModal() {
 function closeModal() {
     showModal.value = false;
 }
+
+function acceptTerms() {
+    acceptedTerms.value = true;
+    closeModal();
+}
+
+function rejectTerms() {
+    toast.error("Você precisa aceitar os termos para continuar.");
+    closeModal();
+}
 </script>
 
 <style scoped>
@@ -142,17 +175,20 @@ function closeModal() {
 
 .modal {
     background: white;
-    padding: 20px;
-    border-radius: 5px;
+    padding: 30px;
+    border-radius: 8px;
     width: 90%;
-    max-width: 500px;
+    max-width: 600px;
+    max-height: 80vh;
+    overflow-y: auto;
     position: relative;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
 }
 
 .close-button {
     position: absolute;
-    top: 10px;
-    right: 10px;
+    top: 15px;
+    right: 15px;
     background: transparent;
     border: none;
     font-size: 20px;
@@ -215,14 +251,48 @@ function closeModal() {
     cursor: pointer;
 }
 
-.close-button {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background: transparent;
+.modal-buttons {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+}
+
+.accept-button {
+    background-color: #28a745;
+    color: white;
+    margin-left: 10px;
+}
+
+.reject-button {
+    background-color: #dc3545;
+    color: white;
+    margin-left: 10px;
+}
+
+
+.accept-button,
+.reject-button {
+    padding: 10px 20px;
     border: none;
-    font-size: 20px;
+    border-radius: 5px;
     cursor: pointer;
-    color: black;
+    transition: background-color 0.3s;
+}
+
+.accept-button:hover {
+    background-color: #218838;
+}
+
+.reject-button:hover {
+    background-color: #c82333;
+}
+
+.modal h2,
+.modal p {
+    text-align: left;
+}
+
+.reduced-space {
+    margin-bottom: -20px;
 }
 </style>
