52d480a61943ea1d13e60156c7646fa9871f9fad
Silas Prado <silas.prado98@gmail.com>
2024-11-24 11:57:30 -0300

    feat: added graph for RFM metrics

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container">
-    <div class="select-container">
-      <h1>Listagem de Clientes</h1>
-      <br><br>
+    <div class="graph-container">
+      <h1>Métricas RFM</h1>
+      <br>
       <Select v-model="selectedCluster" :options="filterOptions" placeholder="Selecione o cluster">
       </Select>
       <TableComponent :data="data" :display-columns="['participant_id', 'R', 'F', 'M', 'Cluster']"
-        :column-names="['Payment', 'Ressência', 'Frequência', 'Valor Monetário (R$)', 'Cluster']" />
+      :column-names="['Payment', 'Ressência', 'Frequência', 'Valor Monetário (R$)', 'Cluster']" />
+      <div class="graph">
+        <bar-chart :chart-data="dataGraph" :options="chartOptions"></bar-chart>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +20,77 @@ import Select from "@/components/Select/Select.vue";
 import TableComponent from "../components/Table/TableComponent.vue";
 import { toast } from "vue3-toastify";
 import { clientApi } from "../services/api";
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    BarController,
+    LineController,
+    PointElement,
+} from "chart.js";
+import { BarChart } from "vue-chart-3";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement, 
+  BarController,
+  LineController
+);
+
+const dataGraph = {
+  labels: ["Cluster 1", "Cluster 2", "Cluster 3", "Cluster 4"], // Eixo X
+  datasets: [
+    {
+      label: "Recência (R)",
+      data: [10, 20, 15, 25], // Valores de R para cada cluster
+      backgroundColor: "#3B82F6",
+      borderColor: "#2563EB",
+      borderWidth: 1,
+    },
+    {
+      label: "Frequência (F)",
+      data: [12, 18, 22, 30], // Valores de F para cada cluster
+      backgroundColor: "#10B981",
+      borderColor: "#059669",
+      borderWidth: 1,
+    },
+    {
+      label: "Valor Monetário (M)",
+      data: [8, 25, 20, 35], // Valores de M para cada cluster
+      backgroundColor: "#F59E0B",
+      borderColor: "#D97706",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const chartOptions = {
+  responsive: true, // Gráfico responsivo
+  maintainAspectRatio: false, // Permite altura personalizada
+  scales: {
+    x: {
+      beginAtZero: true,
+    },
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: true,
+      position: "top",
+    },
+  },
+};
 
 const filterOptions = ref([
   {
